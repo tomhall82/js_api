88a97623e358eb1e4fc44986ebf34439363e360f
tomhall82 <tom.hall82@yahoo.co.uk>
2024-07-28 18:54:34 +0100

    started to add postForm function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,19 @@ const resultsModal = new bootstrap.Modal(
 document
   .getElementById("status")
   .addEventListener("click", (e) => getStatus(e));
+document.getElementById("submit").addEventListener("click", (e) => postForm(e));
+
+async function postForm(e) {
+  const form = new FormData(document.getElementById("checksform"));
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: API_KEY,
+    },
+    body: form,
+  });
+}
 
 async function getStatus(e) {
   const queryString = `${API_URL}?api_key=${API_KEY}`;
