c5442f4f89b860d53d24db1133c2443ecc2f3824
tomhall82 <tom.hall82@yahoo.co.uk>
2024-07-28 19:57:56 +0100

    create displayException function

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,7 @@ async function postForm(e) {
   if (response.ok) {
     displayErrors(data);
   } else {
+    displayException(data);
     throw new Error(data.error);
   }
 }
@@ -57,7 +58,7 @@ function displayErrors(data) {
       results += `<div class="error">${error.error}</div>`;
     }
   }
-  document.getElementById("resultsModalTitle").innerHTML = heading;
+  document.getElementById("resultsModalTitle").innerText = heading;
   document.getElementById("results-content").innerHTML = results;
   resultsModal.show();
 }
@@ -70,6 +71,7 @@ async function getStatus(e) {
   if (response.ok) {
     displayStatus(data);
   } else {
+    displayException(data);
     throw new Error(data.error);
   }
 }
@@ -79,7 +81,18 @@ function displayStatus(data) {
   let results = `<div>Your key is valid until</div>`;
   results += `<div class="key-status">${data.expiry}</div>`;
 
-  document.getElementById("resultsModalTitle").innerHTML = heading;
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
+  resultsModal.show();
+}
+
+function displayException(data) {
+  let heading = `An Exception Occurred`;
+  let results = `<div>The API returned status code ${data.status_code}</div>`;
+  results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+  results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+  document.getElementById("resultsModalTitle").innerText = heading;
   document.getElementById("results-content").innerHTML = results;
   resultsModal.show();
 }
